fe56e90e714fd00285a200f5acfb3e2e4d746e50
Puck <825597202@qq.com>
2020-01-11 22:23:55 +0800

    s3 1.11

--- src/leetcode/TestMain.java
+++ src/leetcode/TestMain.java
@@ -2,6 +2,7 @@ package leetcode;
 
 import leetcode.simple.S1;
 import leetcode.simple.S2;
+import leetcode.simple.S3;
 
 public class TestMain {
 
@@ -9,8 +10,11 @@ public class TestMain {
 //        S1 s1 = new S1();
 //        String s = "({asasdsadasa}";
 //        System.out.println(s1.isValid(s));
-        S2 s2 = new S2();
-        int[] nums = {1,1,1,1,2,2,3};
-        System.out.println(s2.removeDuplicates(nums));
+//        S2 s2 = new S2();
+//        int[] nums = {1,1,1,1,2,2,3};
+//        System.out.println(s2.removeDuplicates(nums));
+        S3 s3 = new S3();
+        int[] nums = {-21,-3,-4,-1,-2,-1,-5,-4};
+        System.out.println(s3.maxSubArray(nums));
     }
 }
--- src/leetcode/simple/S3.java
+++ src/leetcode/simple/S3.java
@@ -1,5 +1,54 @@
 package leetcode.simple;
 
 public class S3 {
-    
+    public int maxSubArray(int[] nums) {
+        //1. 暴力
+//        int len = nums.length;
+//        int max = Integer.MIN_VALUE;
+//        int sum = 0;
+//        for (int i =0; i < len; i++){
+//            sum=0;
+//            for (int j = i; j < len; j++){
+//                sum += nums[j];
+//                max = Math.max(max,sum);
+//            }
+//        }
+//        return max;
+        //2. 动态规划
+//        int len = nums.length;
+//        int dp = nums[0];
+//        int result = dp;
+//        for (int i = 1; i < len;i++){
+//            dp = Math.max(dp + nums[i],nums[i]);
+//            result = Math.max(result,dp);
+//        }
+//        return result;
+        //2. 动态规划 数组
+//        int len = nums.length;
+//        int[] dp = new int[len];
+//        int result = Integer.MIN_VALUE;
+//        for (int i = 1; i < len;i++){
+//            dp[i] = Math.max(dp[i-1] + nums[i],nums[i]);
+//            result = Math.max(result,dp[i]);
+//        }
+//        return result;
+
+        //3. 贪心法
+        int len = nums.length;
+        int result = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int n:nums){
+            sum += n;
+            result = Math.max(sum,result);
+            if (sum < 0){
+                sum = 0;
+            }
+        }
+        return result;
+
+
+        //4. 分治法
+
+
+    }
 }
