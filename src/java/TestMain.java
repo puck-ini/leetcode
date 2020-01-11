9f06d0b6e77209f6dd696272e094899f0f2afb8a
Puck <825597202@qq.com>
2020-01-11 12:53:36 +0800

    start 1.11

---
+++ src/java/TestMain.java
@@ -0,0 +1,8 @@
+package java;
+
+public class TestMain {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/java/simple/S1.java
@@ -0,0 +1,36 @@
+package java.simple;
+
+import java.util.Stack;
+
+public class S1 {
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        char[] chars = s.toCharArray();
+        for(char c: chars){
+            if(equalLeft(c)){
+                stack.push(c);
+            }
+            if (equalRight(c)){
+                if (stack.size() == 0){
+                    return false;
+                }
+                if (!equalChar(stack.peek(), c)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean equalLeft(char a){
+        return (a == '(' || a == '{' || a == '[');
+    }
+    private boolean equalRight(char a){
+        return (a == ')' || a == '}' || a == ']');
+    }
+    private boolean equalChar(char c1, char c2) {
+        return (c1 == '(' && c2 == ')') || (c1 == '[' && c2 == ']') || (c1 == '{' && c2 == '}');
+    }
+
+}
