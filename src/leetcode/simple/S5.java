32d4784dac06b310a8d584d5768987c5bc2c5a87
Puck <825597202@qq.com>
2020-01-12 10:06:26 +0800

    s5 1.12 1

--- src/leetcode/simple/S5.java
+++ src/leetcode/simple/S5.java
@@ -3,6 +3,8 @@ package leetcode.simple;
 
 import leetcode.simple.datatype.TreeNode;
 
+import java.util.LinkedList;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -16,11 +18,40 @@ public class S5 {
     public int maxDepth(TreeNode root) {
         //1.判断节点是否为空
         //2.递归
+//        if (root == null){
+//            return 0;
+//        }
+//        int maxLeftDepth = maxDepth(root.left);
+//        int maxRightDepth = maxDepth(root.right);
+//        return Math.max(maxLeftDepth,maxRightDepth) + 1;
+
+
+        //迭代,BFS层次遍历思想。广度优先算法
         if (root == null){
             return 0;
         }
-        int maxLeftDepth = maxDepth(root.left);
-        int maxRightDepth = maxDepth(root.right);
-        return Math.max(maxLeftDepth,maxRightDepth) + 1;
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int maxDepth = 0;
+        while (!queue.isEmpty()){
+            maxDepth++;
+            int queueSize = queue.size();
+            //将每一层的节点的子节点推入队列
+            for (int i = 0; i < queueSize; i++){
+                TreeNode node = queue.pollFirst();
+                if (node.left != null){
+                    queue.add(node.left);
+                }
+                if (node.right != null){
+                    queue.add(node.right);
+                }
+            }
+
+        }
+        return maxDepth;
+
+
+        //DFS前序遍历思想。深度优先
+
     }
 }
