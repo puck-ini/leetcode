7cf2f6c81dbebcca6ab6aa2d7e99aceacec105fa
Puck <825597202@qq.com>
2020-01-12 15:38:11 +0800

    s5 1.12 2

--- src/leetcode/TestMain.java
+++ src/leetcode/TestMain.java
@@ -3,6 +3,7 @@ package leetcode;
 import leetcode.simple.S1;
 import leetcode.simple.S2;
 import leetcode.simple.S3;
+import leetcode.simple.S5;
 
 public class TestMain {
 
@@ -13,8 +14,8 @@ public class TestMain {
 //        S2 s2 = new S2();
 //        int[] nums = {1,1,1,1,2,2,3};
 //        System.out.println(s2.removeDuplicates(nums));
-        S3 s3 = new S3();
-        int[] nums = {-21,-3,-4,-1,-2,-1,-5,-4};
-        System.out.println(s3.maxSubArray(nums));
+//        S3 s3 = new S3();
+//        int[] nums = {-21,-3,-4,-1,-2,-1,-5,-4};
+//        System.out.println(s3.maxSubArray(nums));
     }
 }
--- src/leetcode/simple/S5.java
+++ src/leetcode/simple/S5.java
@@ -1,6 +1,8 @@
 package leetcode.simple;
 
 
+
+import javafx.util.Pair;
 import leetcode.simple.datatype.TreeNode;
 
 import java.util.LinkedList;
@@ -27,31 +29,51 @@ public class S5 {
 
 
         //迭代,BFS层次遍历思想。广度优先算法
-        if (root == null){
-            return 0;
-        }
-        LinkedList<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        int maxDepth = 0;
-        while (!queue.isEmpty()){
-            maxDepth++;
-            int queueSize = queue.size();
-            //将每一层的节点的子节点推入队列
-            for (int i = 0; i < queueSize; i++){
-                TreeNode node = queue.pollFirst();
-                if (node.left != null){
-                    queue.add(node.left);
-                }
-                if (node.right != null){
-                    queue.add(node.right);
-                }
-            }
-
-        }
-        return maxDepth;
+//        if (root == null){
+//            return 0;
+//        }
+//        LinkedList<TreeNode> queue = new LinkedList<>();
+//        queue.add(root);
+//        int maxDepth = 0;
+//        while (!queue.isEmpty()){
+//            maxDepth++;
+//            int queueSize = queue.size();
+//            //将每一层的节点的子节点推入队列
+//            for (int i = 0; i < queueSize; i++){
+//                TreeNode node = queue.pollFirst();
+//                if (node.left != null){
+//                    queue.add(node.left);
+//                }
+//                if (node.right != null){
+//                    queue.add(node.right);
+//                }
+//            }
+//
+//        }
+//        return maxDepth;
 
 
         //DFS前序遍历思想。深度优先
+        if (root == null) {
+            return 0;
+        }
+        //用于记录每个节点以及节点的深度
+        //TODO
+        LinkedList<Pair<TreeNode, Integer>> stack = new LinkedList<>();
+        stack.push(new Pair<>(root, 1));
+        int maxDepth = 0;
+        while (!stack.isEmpty()){
+            Pair<TreeNode, Integer> pair = stack.pop();
+            TreeNode node = pair.getKey();
+            maxDepth = Math.max(maxDepth, pair.getValue());
+            if (node.left != null){
+                stack.push(new Pair<>(node.left, pair.getValue() + 1));
+            }
+            if (node.right != null){
+                stack.push(new Pair<>(node.right, pair.getValue() + 1));
+            }
+        }
+        return maxDepth;
 
     }
 }
