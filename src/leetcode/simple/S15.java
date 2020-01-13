44b074d8fbbbe4d3018956e773388dfe7ffbf2da
Puck <825597202@qq.com>
2020-01-13 15:18:56 +0800

    s15 1.13 1

--- src/leetcode/simple/S15.java
+++ src/leetcode/simple/S15.java
@@ -3,15 +3,28 @@ package leetcode.simple;
 public class S15 {
     public int hammingWeight(int n) {
 
-        //循环位移
-        int bits = 0;
-        int mask = 1;
-        for (int i =0;i < 32; i++){
-            if ((n & mask) != 0){
-                bits++;
-            }
-            mask <<=1;
+        //1.循环位移
+//        int bits = 0;
+//        int mask = 1;
+//        for (int i =0;i < 32; i++){
+//            if ((n & mask) != 0){
+//                bits++;
+//            }
+//            mask <<=1;
+//        }
+//        return bits;
+
+        //2.
+        /** n = 110
+         *  110&101=100   ++
+         *  100&011=0     ++
+         *  return 2
+         */
+        int sum = 0;
+        while (n!=0){
+            sum++;
+            n &= (n - 1);
         }
-        return bits;
+        return sum;
     }
 }
