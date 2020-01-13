83e366cc4a4e1ce8ca0cfc2154cc4f4e5bd288c1
Puck <825597202@qq.com>
2020-01-13 10:55:45 +0800

    s12 1.13 1

--- src/leetcode/simple/S12.java
+++ src/leetcode/simple/S12.java
@@ -3,7 +3,9 @@ package leetcode.simple;
 public class S12 {
 
 
-    //只需计算5的个数即可
+    /**计算0的个数，一个数乘10位数才会有0出现，而10可以分解为2*5，
+     * 在阶乘中2的个数比5多，因此只需计算5的个数即可
+     */
     public int trailingZeroes(int n) {
         int count = 0;
         while (n > 0){
