c63a69db93b5706e22977f2764febb7473368a7d
Puck <825597202@qq.com>
2020-01-13 14:02:41 +0800

    s13 1.13 1

--- src/leetcode/simple/S13.java
+++ src/leetcode/simple/S13.java
@@ -1,26 +1,51 @@
 package leetcode.simple;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class S13 {
 
     //投票算法
     public int majorityElement(int[] nums) {
         //1.暴力解决
-        int len = nums.length;
-        int majority = len/2;
-        for(int i:nums){
-            int count = 0;
-            for (int j:nums){
-                if (i == j){
-                   count++;
-                }
-            }
-            if (count > majority){
-                return i;
+//        int len = nums.length;
+//        int majority = len/2;
+//        for(int i:nums){
+//            int count = 0;
+//            for (int j:nums){
+//                if (i == j){
+//                   count++;
+//                }
+//            }
+//            if (count > majority){
+//                return i;
+//            }
+//        }
+//        return Integer.MIN_VALUE;
+
+        //2.hashmap，通过hashmap保存所有数以及出现次数
+        Map<Integer,Integer> counts = countNum(nums);
+        Map.Entry<Integer,Integer> entry = null;
+        for (Map.Entry<Integer,Integer> entry1: counts.entrySet()){
+            if (entry == null || entry1.getValue() > entry.getValue()){
+                entry = entry1;
             }
         }
-        return Integer.MIN_VALUE;
+        return entry.getKey();
 
-        //2.hashmap
+    }
 
+
+    //2.hashmap保存所有数以及出现次数
+    private Map<Integer,Integer> countNum(int[] nums){
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int num: nums){
+            if (!map.containsKey(num)){
+                map.put(num,1);
+            }else {
+                map.put(num,map.get(num) + 1);
+            }
+        }
+        return map;
     }
 }
