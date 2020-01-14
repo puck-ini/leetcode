ca5c45db8dfb3e6f6fe4fbccc32f7b2433e07769
Puck <825597202@qq.com>
2020-01-14 21:05:23 +0800

    s18 1.14

---
+++ src/leetcode/simple/S18.java
@@ -0,0 +1,37 @@
+package leetcode.simple;
+
+import leetcode.simple.datatype.ListNode;
+
+import java.util.List;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+/**
+ * 1->2->3->4->5->NULL
+ * 5->4->3->2->1->NULL
+ */
+
+/**
+ * 使用当前节点指针、前一节点指针，记录当前节点以及前一节点，
+ * 将当前节点指向前一节点，之后指针前移
+ */
+public class S18 {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null){
+            ListNode nextNode = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = nextNode;
+        }
+        return pre;
+    }
+}
