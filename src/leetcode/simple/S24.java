3a0ac70e7aef5ab5136d5de3d2aba33f289b1c37
Puck <825597202@qq.com>
2020-02-02 21:50:57 +0800

    s24 2.2 1

---
+++ src/leetcode/simple/S24.java
@@ -0,0 +1,17 @@
+package leetcode.simple;
+
+public class S24 {
+    public boolean isPowerOfFour(int num) {
+        //循环，题目不能用循环和递归
+//        int temp;
+//        while (num != 1){
+//            temp = num;
+//            num = num % 4 ==0 ?num/4:num;
+//            if (temp == num){
+//                return false;
+//            }
+//        }
+//        return true;
+
+    }
+}
