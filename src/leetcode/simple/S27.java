8be31a8f0c62b598f23c1591908b8d47e72a9658
Puck <825597202@qq.com>
2020-02-03 16:30:47 +0800

    s27 2.23 1

--- src/leetcode/simple/S27.java
+++ src/leetcode/simple/S27.java
@@ -2,11 +2,15 @@ package leetcode.simple;
 
 public class S27 {
     public int getSum(int a, int b) {
-        while (b != 0){
-            int temp = a ^ b;
-            b = (a & b) << 1;
-            a = temp;
-        }
-        return a;
+//        while (b != 0){
+//            int temp = a ^ b;
+//            b = (a & b) << 1;
+//            a = temp;
+//        }
+//        return a;
+        if (a == 0){ return b;}
+        if (b == 0) {return a;}
+        return getSum(a ^ b,(a & b) << 1);
+
     }
 }
