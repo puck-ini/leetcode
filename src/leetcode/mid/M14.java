5f2e5085537a290eec75ffdeadbde63ad6d834b7
Puck <825597202@qq.com>
2020-02-12 16:16:36 +0800

    ThreadPoolTest 2.12

---
+++ src/leetcode/mid/M14.java
@@ -0,0 +1,13 @@
+package leetcode.mid;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class M14 {
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+
+    }
+
+}
---
+++ src/leetcode/test/ThreadPoolTest.java
@@ -0,0 +1,16 @@
+package leetcode.test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+//import java.util.concurrent.ThreadPoolExecutor;
+
+public class ThreadPoolTest {
+
+    public static void main(String[] args) {
+//        ExecutorService executorService = Executors.newScheduledThreadPool()
+
+//        ExecutorService executorService1 = new ThreadPoolExecutor()
+
+    }
+}
