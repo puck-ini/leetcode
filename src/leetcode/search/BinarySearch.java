c2e73c31b68ee24a2c8dcb74d04a37c9ec1f987b
Puck <825597202@qq.com>
2020-02-13 15:25:03 +0800

    binary search 2.13

---
+++ src/leetcode/search/BinarySearch.java
@@ -0,0 +1,28 @@
+package leetcode.search;
+
+public class BinarySearch {
+
+    public static int binarySearch(int[] nums, int data){
+        int min = 0;
+        int max = nums.length - 1;
+        int mid;
+        while (min <= max){
+            //这种写法可以防止溢出， (max + mid) / 2 可能会溢出
+            mid = min + (max - min) / 2;
+            if (nums[mid] > data){
+                max = mid - 1 ;
+            }else if (nums[mid] < data){
+                min = mid + 1;
+            }else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3,4,5,6,7,8,9};
+        System.out.println(binarySearch(nums,5));
+    }
+}
