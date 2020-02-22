900f3a984e571b447310035eca3618fed5f04797
Puck <825597202@qq.com>
2020-02-22 15:39:34 +0800

    m12 2.22

--- src/leetcode/mid/M12.java
+++ src/leetcode/mid/M12.java
@@ -1,12 +1,47 @@
 package leetcode.mid;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 
 //TODO
 public class M12 {
+    //回溯算法
+    private List<List<Integer>> res = new ArrayList<>();
+    private int[] candidates;
+    private int len;
+
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        return new ArrayList<>();
+
+        int len = candidates.length;
+        if (len == 0) {
+            return res;
+        }
+        this.len = len;
+        this.candidates = candidates;
+        Arrays.sort(this.candidates);
+        backtrace(target, 0, new Stack<>());
+        return res;
     }
+
+    private void backtrace(int residue, int start, Stack<Integer> pre) {
+        if (residue < 0) {
+            return;
+        }
+        if (residue == 0) {
+            res.add(new ArrayList<>(pre));
+            return;
+        }
+        for (int i = start; i < len; i++) {
+            if (i > start && candidates[i] == candidates[i - 1]){
+                continue;
+            }
+            pre.add(candidates[i]);
+            backtrace(residue - candidates[i], i + 1, pre);
+            pre.pop();
+        }
+    }
+
 }
