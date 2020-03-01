418bc1e6c5a7c1875a652e7733219fb52e259892
Puck <825597202@qq.com>
2020-03-01 14:00:07 +0800

    m19 3.1

--- src/leetcode/mid/M19.java
+++ src/leetcode/mid/M19.java
@@ -1,5 +1,39 @@
 package leetcode.mid;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
 public class M19 {
 
+    private class  Interval{
+        public int start;
+        public int end;
+    }
+    private class IntervalComparator implements Comparator<Interval>{
+
+        @Override
+        public int compare(Interval o1, Interval o2) {
+            return o1.start < o2.start ? -1 : o1.start == o2.start ? 0 : 1;
+        }
+
+    }
+    public List<Interval> merge(List<Interval> intervals) {
+        Collections.sort(intervals,new IntervalComparator());
+        LinkedList<Interval> merged = new LinkedList<Interval>();
+        for (Interval interval : intervals) {
+            // if the list of merged intervals is empty or if the current
+            // interval does not overlap with the previous, simply append it.
+            if (merged.isEmpty() || merged.getLast().end < interval.start) {
+                merged.add(interval);
+            }
+            // otherwise, there is overlap, so we merge the current and previous
+            // intervals.
+            else {
+                merged.getLast().end = Math.max(merged.getLast().end, interval.end);
+            }
+        }
+        return merged;
+    }
 }
