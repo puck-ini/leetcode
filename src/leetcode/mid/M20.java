bcf8f5c46251909090dea1f64f801b0cfbb4dd30
Puck <825597202@qq.com>
2020-03-04 16:45:35 +0800

    m20 3.4

--- src/leetcode/mid/M20.java
+++ src/leetcode/mid/M20.java
@@ -1,7 +1,33 @@
 package leetcode.mid;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class M20 {
     public String getPermutation(int n, int k) {
-        return null;
+        int[] factorials = new int[n];
+        List<Integer> nums = new ArrayList() {{add(1);}};
+
+        factorials[0] = 1;
+        for(int i = 1; i < n; ++i) {
+            // generate factorial system bases 0!, 1!, ..., (n - 1)!
+            factorials[i] = factorials[i - 1] * i;
+            // generate nums 1, 2, ..., n
+            nums.add(i + 1);
+        }
+
+        // fit k in the interval 0 ... (n! - 1)
+        --k;
+
+        // compute factorial representation of k
+        StringBuilder sb = new StringBuilder();
+        for (int i = n - 1; i > -1; --i) {
+            int idx = k / factorials[i];
+            k -= idx * factorials[i];
+
+            sb.append(nums.get(idx));
+            nums.remove(idx);
+        }
+        return sb.toString();
     }
 }
